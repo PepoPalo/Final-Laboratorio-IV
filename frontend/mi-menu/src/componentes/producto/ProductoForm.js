0ebb65c60eeb05cfc1a42402b5cf05e812128932
Pedro Palomino <pedropalominoaraoz@gmail.com>
2021-03-03 02:00:55 -0300

    cambios gráficos em ProductoForm

--- frontend/mi-menu/src/componentes/producto/ProductoForm.js
+++ frontend/mi-menu/src/componentes/producto/ProductoForm.js
@@ -57,33 +57,35 @@ function handleOnChange(event, campo) {
             {!id && <h1>Nuevo producto</h1>}
             <form onSubmit={(event) => guardar(event)}>
                 <div className="form-group">
-                    <label>Tipo</label>
-                    <input type="text" className="form-control" value={producto.tipo} onChange={(event) => handleOnChange(event, 'tipo')} />
-                </div>
-                <div className="form-group">
                     <label>Descripcion</label>
                     <input type="text" className="form-control" value={producto.descripcion} onChange={(event) => handleOnChange(event, 'descripcion')} />
+                </div>
+                <div className="row text-center align-items-center">
+                    <div className="form-group col-2">
+                        <label>Tipo</label>
+                        <select className="form-control" onChange={(event) => handleOnChange(event, 'tipo')}>
+                            <option selected value={producto.tipo}>{producto.tipo}</option>
+                            <option value="Plato">Plato</option>
+                            <option value="Bebida">Bebida</option>
+                        </select>
+                    </div>
+                    
+                    
+                    <div className="form-group col-2">
+                        <label >Costo</label>
+                        <input type="text" className="form-control" placeholder="00.00" value={producto.costo} onChange={(event) => handleOnChange(event, 'costo')} />
+                    </div>
 
-                </div>
-                <div className="form-group">
-                    <label>Costo</label>
-                    <input type="text" className="form-control" value={producto.costo} onChange={(event) => handleOnChange(event, 'costo')} />
-                </div>
+                    <div className="form-group col-3">
+                        <label >Porcentaje de ganancia</label>
+                        <input type="text" className="form-control" placeholder="0" value={producto.porcentaje_ganancia} onChange={(event) => handleOnChange(event, 'porcentaje_ganancia')} />
+                    </div>
 
-                <div className="form-group">
-                    <label>Porcentaje de ganancia</label>
-                    <input type="text" className="form-control" value={producto.porcentaje_ganancia} onChange={(event) => handleOnChange(event, 'porcentaje_ganancia')} />
+                    <div className="col-3">
+                        <button type="submit" className="btn btn-primary mr-2">Aceptar</button>
+                        <button onClick={() => history.push("/productos/")} className="btn btn-danger">Cancelar</button>
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label>Porcentaje de venta</label>
-                    <input type="text" className="form-control" value={producto.porcentaje_venta} onChange={(event) => handleOnChange(event, 'porcentaje_venta')} />
-                </div>
-
-                <div className="float-right">
-                    <button type="submit" className="btn btn-primary mr-2">Aceptar</button>
-                    <button onClick={() => history.push("/productos/")} className="btn btn-primary">cancelar</button>
-                </div>
-
 
             </form>
             
